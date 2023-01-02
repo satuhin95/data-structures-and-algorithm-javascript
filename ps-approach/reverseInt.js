becc8f61dac7fa022e01f29333dd40a3fb6eb0a6
satuhin95 <saifcm95@gmail.com>
2023-01-02 22:08:03 +0600

    reverseInt

--- ps-approach/reverseInt.js
+++ ps-approach/reverseInt.js
@@ -1,14 +1,20 @@
 
 
 // reverse integer 
+const isSigned32bitInteger = (integer) => {
+    return (integer > Math.pow(2, 31) * -1) && (integer < Math.pow(2,31) - 1);
+};
 
 function reverseInt(num){
+    let result= 0;
     let revNum = num.toString().split('').reverse().join('');
     if(revNum.endsWith('-')){
         revNum = '-' + revNum.slice(0, revNum.length - 1 )
     }
 
-    return parseInt(revNum);
+    result= parseInt(revNum);
+    return isSigned32bitInteger(result) ? result : 0;
+
 }
 
 console.log(reverseInt(-60))
